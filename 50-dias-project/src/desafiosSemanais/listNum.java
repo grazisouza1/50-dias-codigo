609c4cf4d4fdca6fbe281798bee472e546db6cdc
grazisouza1 <graziisouza0127@gmail.com>
2026-01-19 12:14:32 -0300

    Criando teste de entrada para o usuário

---
+++ 50-dias-project/src/desafiosSemanais/listNum.java
@@ -0,0 +1,14 @@
+package desafiosSemanais;
+
+import java.util.Scanner;
+
+public class listNum {
+
+    public static void main (String[] args) {
+        int[] nums;
+
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt(); //Dispara um erro caso o usuário tente usar um tipo diferente de inteiro
+    }
+}
