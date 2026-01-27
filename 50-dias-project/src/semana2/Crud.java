44677981a54ba37cc2ce18d14919e89b2a0337d7
grazisouza1 <graziisouza0127@gmail.com>
2026-01-27 11:44:50 -0300

    feat:Iniciando entrada do usuário e tratamendo de erros

--- 50-dias-project/src/semana2/Crud.java
+++ 50-dias-project/src/semana2/Crud.java
@@ -1,7 +1,38 @@
 package semana2;
 
+import java.util.Scanner;
+
 public class Crud {
     public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        while(true) {
+            System.out.println("======= Lista de Tarefas =======\n");
+            System.out.println("1. Criar nova tarefa");
+            System.out.println("2. Ver tarefas");
+            System.out.println("3. Editar tarefa");
+            System.out.println("4. Deletar tarefa");
+            System.out.println("5. Sair\n");
+            System.out.println("================================\n");
+
+
+            System.out.println("Qual opção deseja? ");
+
+            String entrada = scanner.nextLine();
+
+            try {
+                int opcao = Integer.parseInt(entrada);
+                System.out.println("\nOpção escolhida: " + opcao + "\n");
+            } catch (NumberFormatException e) {
+                System.out.println("\nEntrada inválida\n");
+            }
+
+            //Tratamento de erros de entrada
+            //if (opcao < 1 || opcao > 5) {
+              //  System.out.println("\nNúmero de opção inválido\n");
+                //continue;
+            //}
+
+        }
     }
 }
