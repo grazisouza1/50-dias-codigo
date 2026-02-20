55eb539647934374aba6fca4fb27da3962488e43
grazisouza1 <graziisouza0127@gmail.com>
2026-02-20 09:09:05 -0300

    feat: Criando testes para menu e CharacterService

--- OnePieceSearch/src/test/java/com/semana4/onepiecesearch/menu/MenuTest.java
+++ OnePieceSearch/src/test/java/com/semana4/onepiecesearch/menu/MenuTest.java
@@ -3,6 +3,9 @@ package com.semana4.onepiecesearch.menu;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.semana4.onepiecesearch.dto.CharacterDto;
 import com.semana4.onepiecesearch.service.CharacterService;
+import com.semana4.onepiecesearch.service.CrewService;
+import com.semana4.onepiecesearch.service.EpisodeService;
+import com.semana4.onepiecesearch.service.SagaService;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.client.RestTemplate;
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,22 +14,20 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MenuTest {
 
     @Test
-    void deveRetornarZoroMesmoComDoisNomes() throws JsonProcessingException {
-        RestTemplate restTemplate = new RestTemplate();
-        CharacterService service = new CharacterService(restTemplate);
+    public void entradaDoUserNaoPodeSerVazia() {
+        Menu menu = new Menu(null, null, null, null);
 
-        CharacterDto resultado = service.searchByName("Roronoa Zoro");
+        Integer resultado = menu.processInput("");
 
-        assertEquals("Roronoa Zoro", resultado.getName());
+        assertNull(resultado);
     }
 
     @Test
-    void deveRetornarLuffyMesmoComMaisDeUmNome() throws JsonProcessingException {
-        RestTemplate restTemplate = new RestTemplate();
-        CharacterService service = new CharacterService(restTemplate);
+    public void deveRetornarNumeroValido(){
+        Menu menu = new Menu(null, null, null, null);
 
-        CharacterDto resultado = service.searchByName("Monkey D Luffy");
+        Integer resultado = menu.processInput("2");
 
-        assertEquals("Monkey D Luffy", resultado.getName());
+        assertEquals("2", resultado.toString());
     }
 }
