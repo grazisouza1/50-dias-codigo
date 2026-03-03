482c737429a71bc21a477a38dff85b6b53cf3c5a
grazisouza1 <graziisouza0127@gmail.com>
2026-03-03 08:31:37 -0300

    refactor: Adicionando funções ao AccountDao

--- BankApp/src/main/java/com/semana6/bankapp/dao/AccountDao.java
+++ BankApp/src/main/java/com/semana6/bankapp/dao/AccountDao.java
@@ -1,6 +1,7 @@
 package com.semana6.bankapp.dao;
 
 import com.semana6.bankapp.dto.AccountDto;
+import com.semana6.bankapp.dto.CustomerDto;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
@@ -26,6 +27,62 @@ public class AccountDao {
 
             pstm.executeUpdate();
         }
-        }
     }
+
+    public AccountDto buscarContaPorClienteId(int customerId) throws SQLException {
+
+        Connection conn = new ConexaoDB().connectDB();
+
+        String sql = "SELECT * FROM accounts WHERE customer_id = ?";
+
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setLong(1, customerId);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                AccountDto conta = new AccountDto();
+                conta.setId(rs.getInt("account_id"));
+                conta.setCustomer_id(rs.getInt("customer_id"));
+                conta.setBalance(rs.getFloat("balance"));
+                return conta;
+            }
+        }
+
+        return null;
+    }
+
+    public void atualizarSaldo (int accountId, float novoSaldo) throws SQLException {
+        String sql = "UPDATE accounts SET balance = ? WHERE account_id = ?";
+
+        Connection conn = new ConexaoDB().connectDB();
+        PreparedStatement pstm = conn.prepareStatement(sql);
+        pstm.setFloat(1, novoSaldo);
+        pstm.setInt(2, accountId);
+
+        pstm.executeUpdate();
+    }
+
+    public AccountDto buscarContaPorCustomerId(int id) throws SQLException {
+        Connection conn = new ConexaoDB().connectDB();
+
+        String sql = "SELECT * FROM accounts WHERE customer_id = ?";
+
+        try (PreparedStatement pstm = conn.prepareStatement(sql)) {
+            pstm.setInt(1, id);
+
+            ResultSet rs = pstm.executeQuery();
+
+            if (rs.next()) {
+                AccountDto conta = new AccountDto();
+                conta.setId(rs.getInt("account_id"));
+                conta.setCustomer_id(rs.getInt("customer_id"));
+                conta.setBalance(rs.getFloat("balance"));
+                return conta;
+            }
+        }
+
+        return null;
+    }
+}
 
